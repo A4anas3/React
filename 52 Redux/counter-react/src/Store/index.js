0287edd718c21e7009d10c6d5921fff8c7a64c2b
Mohd Anas <a4anaskhan3@gmail.com>
2025-10-12 15:41:39 +0530

    REDUX

--- 52 Redux/counter-react/src/Store/index.js
+++ 52 Redux/counter-react/src/Store/index.js
@@ -1,3 +1,29 @@
 import {createStore} from "redux";
 
-const counterStore=createStore()
+
+
+const INITIAL_VALUE={
+
+    counter: 10,
+    privacy : false
+}
+
+const counterReducer= (store=INITIAL_VALUE,action)=>{
+    if(action.type==='INCREMENT'){
+        return {counter:store.counter+1}
+
+    }else if(action.type==='DECREMENT'){
+        return {counter: store.counter-1}
+
+    }else if(action.type==='ADD'){
+        return {counter: store.counter+Number(action.payload.num)}
+    }else  if(action.type==='SUB'){
+         return {counter: store.counter-Number(action.payload.num)}
+    }else if(action.type==='PRIVACY'){
+        return {privacy:!store.privacy}
+    }
+    return store;
+}
+
+const counterStore=createStore(counterReducer);
+export default counterStore;
